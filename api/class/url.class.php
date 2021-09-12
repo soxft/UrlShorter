dda58638fd4ba22fd1ec36adfbbc5d5dec597ba3
xcsoft <email@xcsoft.top>
2021-09-12 20:03:58 +0800

    bug fixed

--- api/class/url.class.php
+++ api/class/url.class.php
@@ -11,11 +11,25 @@ class urllib
     function __construct($conn)
     {
         $this->conn = $conn;
-        $this->ifUserDefine = false; //是否用户自定义短链
+        $this->ifUserDefine = true; //是否用户自定义短链
+        $this->AlreadyExists = false; //是否已经存在
+    }
+
+    /**
+     * 获取短链对应的长链接
+     * @param $short
+     * @return array
+     */
+    function getLongUrl(string $short): array
+    {
+        return [];
     }
 
     /**
      * 生成短链接
+     * @param $url
+     * @param $short
+     * @return array
      */
     public function getShort(string $url, string $short): array
     {
@@ -28,7 +42,7 @@ class urllib
             if (!$short) $short = $this->createShort();
             if (!preg_match('/^[a-zA-Z0-9]{0,}$/', $short)) return ['code' => 1002, 'msg' => '短链接必须为字母,数字,组合', 'url' => ''];
             if (strlen($short) > 10 || strlen($short) < 4) return ['code' => 1003, 'msg' => '短链接长度必须在4-10位', 'url' => ''];
-            if ($this->checkShort($short)) return ['code' => 1006, 'msg' => '自定义短链接已存在', 'url' => ''];
+            if ($this->checkShort($url, $short)) return ['code' => 1006, 'msg' => '自定义短链接已存在', 'url' => ''];
             return ['code' => 0, 'msg' => 'success', 'url' => WEBURL . $this->addShorter($url, trim($short))];
         } catch (Exception $e) {
             $arr = ['code' => 99999, 'msg' => '系统错误,请稍后再试', 'url' => ''];
@@ -42,11 +56,16 @@ class urllib
      * @param string $short
      * @return bool
      */
-    public function checkShort(string $short): bool
+    public function checkShort(string $url, string $short): bool
     {
-        $sel = $this->conn->prepare("SELECT `time` FROM `shorter` WHERE `short` = ? ");
+        $sel = $this->conn->prepare("SELECT `url` FROM `shorter` WHERE `short` = ? ");
         $sel->execute([$short]);
-        if ($sel->fetch()) return true;
+        $res = $sel->fetch();
+        if ($url == $res['url']) :
+            $this->AlreadyExists = true;
+            return false;
+        endif;
+        if ($res) return true;
         return false;
     }
 
@@ -62,7 +81,7 @@ class urllib
         if (!isset($urlArr['host'])) return 'Err';
         $Punycode = new Punycode();
         $urlArr['host'] = $Punycode->encode($urlArr['host']);
-        $url = tool::unparse_url($urlArr);
+        $url = Tool::unparse_url($urlArr);
         return $url;
     }
 
@@ -74,6 +93,7 @@ class urllib
      */
     private function addShorter(string $url, string $short)
     {
+        if ($this->AlreadyExists) return $short;
         //检测是否已经存在
         if (!$this->ifUserDefine) {
             $sel = $this->conn->prepare("SELECT `short` FROM `shorter` WHERE `url` = ? ");
@@ -82,7 +102,7 @@ class urllib
             if ($res) return $res['short'];
         }
         $insert = $this->conn->prepare("INSERT INTO `shorter` VALUES ('0', ? , ? , ? , ? )");
-        $insert->execute([$short, $url, time(), tool::getIp()]);
+        $insert->execute([$short, $url, time(), Tool::getIp()]);
         if ($insert->rowCount() === 0) throw new \Exception('写入短链接失败');
         return $short;
     }
@@ -94,13 +114,13 @@ class urllib
     private function createShort(): string
     {
         while (True) { //重复检测
-            $short = tool::randStr(is_numeric(SHORT_LEN) ? SHORT_LEN : mt_rand(4, 6));
+            $short = Tool::randStr(is_numeric(SHORT_LEN) ? SHORT_LEN : mt_rand(4, 6));
             $checkIfExists = $this->conn->prepare("SELECT * FROM `shorter` WHERE `short` = :short ");
             $checkIfExists->bindParam(':short', $short);
             $checkIfExists->execute();
             if ($checkIfExists->rowCount() == 0) break;
         }
-        $this->ifUserDefine = true;
+        $this->ifUserDefine = false;
         return $short;
     }
 }
--- api/main.php
+++ api/main.php
@@ -23,7 +23,7 @@ $return = $main->run();
 //根据encode类型返回
 if (($_REQUEST['encode'] ?? 'json') == 'xml') {
     header("content-type:text/xml; charset=utf-8");
-    echo tool::xml_encode($return);
+    echo Tool::xml_encode($return);
 } else {
     header("content-type:text/json; charset=utf-8");
     echo json_encode($return, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
